4e1ee43ea1b4573bd1b50a2c03a70767cc6646cf
Haoran Feng <harrifeng@gmail.com>
2018-03-31 08:16:23 +0000

    new way

---
+++ 002-mongo-http-server/insert_member.js
@@ -0,0 +1,7 @@
+db.members.insertMany(
+  {"member_id": 11, "telephone": 13923567841},
+  {"member_id": 21, "telephone": 13923567842},
+  {"member_id": 31, "telephone": 13923567843},
+  {"member_id": 41, "telephone": 13923567844},
+  {"member_id": 51, "telephone": 13923567845},
+)

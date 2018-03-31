fb796ec1cddb7c50369d542a8bca696bc1e9ff17
Haoran Feng <harrifeng@gmail.com>
2018-03-31 08:11:10 +0000

    new import

---
+++ 002-mongo-http-server/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type MemberReport struct {
+	MemberID  int64  `json:"member_id"`
+	Telephone string `json:"telephone"`
+}
+
+type InputBody struct {
+	MemberID int64 `json:"member_id"`
+}
+
+type OutputBody struct {
+	Code    int            `json:"code"`
+	Message string         `json:"message"`
+	Data    []MemberReport `json: "data"`
+}
+
+func MemberRelationHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	output := OutputBody{0, "", []MemberReport{}}
+
+	switch r.Method {
+	case "POST":
+		w.WriteHeader(200)
+
+		decoder := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		var t []InputBody
+		err := decoder.Decode(&t)
+		if err != nil {
+			w.WriteHeader(400)
+			output.Message = "Incorrect json format"
+			json.NewEncoder(w).Encode(output)
+			break
+		}
+		for _, one := range t {
+			output.Data = append(output.Data, MemberReport{one.MemberID, "12345678912"})
+		}
+
+		json.NewEncoder(w).Encode(output)
+
+	case "HEAD":
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(output)
+	default:
+		w.WriteHeader(400)
+		output.Message = "Unsupport HTTP Method"
+		json.NewEncoder(w).Encode(output)
+	}
+}
+
+func main() {
+
+	fmt.Println()
+
+	http.HandleFunc("/member_relation", MemberRelationHandler)
+	http.ListenAndServe("0.0.0.0:5679", nil)
+	os.Exit(0)
+}
